3bf20bb8fe4316d265be90471d04b93d32cf3dbe
Walid Rijal Awali <108478845+walid666-afk@users.noreply.github.com>
2023-12-15 03:30:32 +0700

    Add files via upload

---
+++ UAS_ML/_🏠_Home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home"
+)
+# Logo
+
+
+st.title("Selamat Datang di Diabetes Mellitus Diagnosis System")
+
+st.image("logo.jpeg", width=500)
+st.write("---")
+
+st.subheader("Apa itu Dibetes Mellitus ?")
+
+st.write("Diabetes melitus adalah sebuah penyakit metabolik yang disebabkan oleh karena ketidakmampuan tubuh dalam memanfaatkan insulin atau kekurangan hormon insulin, akibatnya kadar gula di dalam darah tidak dapat terkendali.")
+
+st.write("Penyakit diabetes mellitus sekarang menyerang disemua kalangan usia, baik muda maupun lansia. Lebih dari 1,2 juta anak di seluruh dunia menderita penyakit diabetes mellitus. Diabetes mellitus tetap menjadi salah satu penyakit paling mematikan di dunia. Dunia mengalami 6,7 juta kematian akibat diabetes mellitus pada tahun 2021.")
+
+st.write("World Health Organization menyatakan bahwa jumlah orang yang menderita diabetes melitus terus meningkat hingga empat juta kali lebih besar dari tahun sebelumnya. Satu juta orang mati setiap tahun akibat diabetes melitus. Penyakit diabetes melitus dapat disembuhkan jika penderita menyadari dan mengikuti pengobatan sejak awal sebelum menjadi lebih parah. Oleh karena itu, masyarakat membutuhkan informasi tentang faktor-faktor yang menyebabkan diabetes melitus, dan juga gejala gejala yang terjadi.")
+
+st.write("Dengan jumlah penderita yang terus meningkat di setiap tahunnya membuat dari pihak rumah sakit menjadi kewalahan dalam memberikan pelayan bagi pasien yang ingin mengetahui apakah dirinya menderita penyakit diabetes mellitus atau tidak.")
+
+st.write("Dengan adanya permasalahan diatas, kami membuat suatu sistem untuk mendiagnosa penyakit Diabetes Mellitus, silahkan Diagnosis penyakit anda.")
+
+st.write("---")
+
+st.write("By Walid Rijal Awali")
+st.write("© Copyright 2023.")
